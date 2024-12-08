caa157b4194bd525d9f4161bad7ed4ebf8cdd7be
Alexander-ZAG182 <hado18190@gmail.com>
2024-12-08 15:34:38 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author tolstoy = new Author("Лев", "Толстой");
+        Author gogol = new Author("Николай", "Гоголь");
+
+        Book warAndPeace = new Book("Война и мир", tolstoy, 1869);
+        Book deadSouls = new Book("Мертвые души", gogol, 1842);
+
+        System.out.println(warAndPeace.getTitle() + " " + warAndPeace.getPublishingYear());
+        System.out.println(deadSouls.getTitle() + " " + deadSouls.getPublishingYear());
+
+        warAndPeace.setPublishingYear(1995);
+        System.out.println(warAndPeace.getTitle() + " " + warAndPeace.getPublishingYear());
+
     }
 }
